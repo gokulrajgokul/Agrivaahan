849802861ed5302077ecad24ea494a56fb0c6904
gokulrajgokul <gokul26.cbcs@gmail.com>
2025-04-19 18:21:54 +0530

    slider

--- Agrivehicles/views.py
+++ Agrivehicles/views.py
@@ -13,7 +13,8 @@ from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 def index(request):
-	return render(request,'index.html')
+    vehicles = Vehicle.objects.all()
+    return render(request,'index.html',{'carousel_images': vehicles})
 def about(request):
     return render(request,'about.html ')
 
