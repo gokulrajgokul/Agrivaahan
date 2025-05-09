3bd08b7a0de30f202940d8a808d8fdd87c465a19
gokulrajgokul <gokul26.cbcs@gmail.com>
2025-05-09 15:21:38 +0530

    new

--- Agriproject/settings.py
+++ Agriproject/settings.py
@@ -23,8 +23,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = False
 # ALLOWED_HOSTS = ["*"]
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
-
+ALLOWED_HOSTS = ['agrivaahan-agricultural-equipments.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
